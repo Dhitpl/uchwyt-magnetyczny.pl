9a65a76140a284263c2325284af58b56d3ea6e6a
Ingram Kalina <contact@imaxart.com>
2024-09-10 12:25:42 +0200

    feat: typography usage on home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,7 @@
 import { getTranslations } from 'next-intl/server'
 
+import { Typography } from '~/components'
+
 import type { PageProps } from '~/types'
 
 export async function generateMetadata({ params: { locale } }: PageProps) {
@@ -13,5 +15,46 @@ export async function generateMetadata({ params: { locale } }: PageProps) {
 }
 
 export default function HomePage() {
-  return <div>strona główna</div>
+  return (
+    <div className='flex flex-col gap-2'>
+      <Typography variant='display' size='lg'>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+      <Typography variant='display' size='md'>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+      <Typography variant='display' size='sm'>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+
+      <Typography variant='headline' level={1}>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+      <Typography variant='headline' level={2}>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+      <Typography variant='headline' level={3}>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+      <Typography variant='headline' level={4}>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+      <Typography variant='headline' level={5}>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+      <Typography variant='headline' level={6}>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+
+      <Typography variant='label' size='lg'>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+      <Typography variant='label' size='md'>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+      <Typography variant='label' size='sm'>
+        The quick brown fox jumps over the lazy dog
+      </Typography>
+    </div>
+  )
 }
