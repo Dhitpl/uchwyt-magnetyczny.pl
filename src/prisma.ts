fd42009d89eb625e1b87dc0290a64eaa205a8d46
Ingram Kalina <contact@imaxart.com>
2024-09-14 13:47:35 +0200

    chore: disable eslint for prisma

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
   return new PrismaClient()
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 
 const globalForPrisma = globalThis as unknown as {
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
   prisma: PrismaClientSingleton | undefined
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
