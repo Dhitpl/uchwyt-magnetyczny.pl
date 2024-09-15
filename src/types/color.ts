0666ca44f9acb9b33c3b1ded609fcfca693f4413
Ingram Kalina <contact@imaxart.com>
2024-09-16 01:50:33 +0200

    feat: define color types

---
+++ src/types/color.ts
@@ -0,0 +1,69 @@
+type HexDigit =
+  | '0'
+  | '1'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | 'a'
+  | 'b'
+  | 'c'
+  | 'd'
+  | 'e'
+  | 'f'
+
+type Hex3 = `${HexDigit}${HexDigit}${HexDigit}`
+
+export type HexColor<T extends string> =
+  Lowercase<T> extends `#${Hex3}`
+    ? T
+    : Lowercase<T> extends `#${Hex3}${infer Rest}`
+      ? Rest extends Hex3
+        ? T
+        : never
+      : never
+
+export type ColorShade =
+  | 50
+  | 100
+  | 200
+  | 300
+  | 400
+  | 500
+  | 600
+  | 700
+  | 800
+  | 900
+  | 950
+
+export type ColorPalette =
+  | 'slate'
+  | 'gray'
+  | 'zinc'
+  | 'neutral'
+  | 'stone'
+  | 'red'
+  | 'orange'
+  | 'amber'
+  | 'yellow'
+  | 'lime'
+  | 'green'
+  | 'emerald'
+  | 'teal'
+  | 'cyan'
+  | 'sky'
+  | 'blue'
+  | 'indigo'
+  | 'violet'
+  | 'purple'
+  | 'fuchsia'
+  | 'pink'
+  | 'rose'
+
+export type TokenColor = `${ColorPalette}-${ColorShade}`
+
+export type Color<T extends string = string> = TokenColor | HexColor<T>
