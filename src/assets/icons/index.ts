5673dd087ecb7a862f054ed45af2c90b24af1637
Alan Ostrowski <alan.ostrowski2004@gmail.com>
2024-09-25 12:28:05 +0200

    fix: after rebase

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,8 +1,15 @@
-export default {
-  apple: () => import('./apple'),
-  facebook: () => import('./facebook'),
-  google: () => import('./google'),
-  cart: () => import('./cart'),
-  gmail: () => import('./gmail'),
-  whatsapp: () => import('./whatsapp'),
+import Apple from './apple'
+import Cart from './cart'
+import Facebook from './facebook'
+import Gmail from './gmail'
+import Google from './google'
+import Whatsapp from './whatsapp'
+
+export const icons = {
+  apple: Apple,
+  cart: Cart,
+  facebook: Facebook,
+  gmail: Gmail,
+  google: Google,
+  whatsapp: Whatsapp,
 }
