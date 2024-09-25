80a841117a3c3bbddfbf5776f311021cc30618c4
ingrqm <contact@imaxart.com>
2024-09-25 11:00:53 +0200

    feat: adjust breakpoints

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,7 +1,11 @@
 export const breakpoints = {
-  sm: '640px',
-  md: '768px',
-  lg: '1024px',
-  xl: '1280px',
-  '2xl': '1536px',
+  xs: '0px',
+  sm: '375px',
+  md: '576px',
+  lg: '768px',
+  xl: '991px',
+  '2xl': '1200px',
+  '3xl': '1440px',
 } as const
+
+export type Breakpoint = keyof typeof breakpoints
