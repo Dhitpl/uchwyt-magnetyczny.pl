82aec9b09e1d22cfe35899d74c30bd15d38d3c63
ingrqm <contact@imaxart.com>
2024-09-25 11:00:40 +0200

    feat: use breakpoint hook

--- src/hooks/use-breakpoint.ts
+++ src/hooks/use-breakpoint.ts
@@ -1,28 +1,85 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
-import { breakpoints } from '~/styles/breakpoints'
+import { type Breakpoint, breakpoints } from '~/styles'
 
-type UseBreakpointProps = {
-  breakpoint: keyof typeof breakpoints
+type UseIsBreakpointProps = {
+  breakpoint: Breakpoint
 }
 
-export const useBreakpoint = ({ breakpoint }: UseBreakpointProps) => {
-  const [isMatch, setIsMatch] = useState(false)
+/**
+ * Custom hook to determine if the current window width matches a specified breakpoint.
+ *
+ * @param {UseIsBreakpointProps} props - The properties object.
+ * @param {Breakpoint} props.breakpoint - The breakpoint to match against.
+ * @returns {boolean} - A boolean indicating if the current window width matches the specified breakpoint.
+ */
+export const useIsBreakpoint = ({ breakpoint }: UseIsBreakpointProps) => {
+  const mediaQuery = useMemo(
+    () => window.matchMedia(`(min-width: ${breakpoints[breakpoint]})`),
+    [breakpoint],
+  )
+
+  const [isMatch, setIsMatch] = useState(mediaQuery.matches)
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia(
-      `(min-width: ${breakpoints[breakpoint]})`,
-    )
-
-    const handleChange = () => setIsMatch(mediaQuery.matches)
+    const handleChange = () => {
+      if (mediaQuery.matches !== isMatch) {
+        setIsMatch(mediaQuery.matches)
+      }
+    }
 
     mediaQuery.addEventListener('change', handleChange)
-    handleChange()
 
     return () => mediaQuery.removeEventListener('change', handleChange)
-  }, [breakpoint])
+  }, [mediaQuery, isMatch])
 
   return isMatch
+}
+
+/**
+ * Custom hook to get the current breakpoint based on the window width.
+ *
+ * @returns {Breakpoint} - The current breakpoint.
+ */
+export const useBreakpoint = () => {
+  const [breakpoint, setBreakpoint] = useState<Breakpoint>('xs')
+
+  useEffect(() => {
+    const breakpointsKeys = Object.keys(breakpoints) as Breakpoint[]
+    let resizeTimeout: NodeJS.Timeout | null = null
+
+    const getBreakpoint = () => {
+      const newBreakpoint = breakpointsKeys
+        .reverse()
+        .find(
+          key => window.matchMedia(`(min-width: ${breakpoints[key]})`).matches,
+        )
+
+      if (newBreakpoint && newBreakpoint !== breakpoint) {
+        setBreakpoint(newBreakpoint)
+      }
+    }
+
+    // Debouncing resize event to improve performance
+    const handleResize = () => {
+      if (resizeTimeout) {
+        clearTimeout(resizeTimeout)
+      }
+      resizeTimeout = setTimeout(getBreakpoint, 150)
+    }
+
+    getBreakpoint()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      if (resizeTimeout) {
+        clearTimeout(resizeTimeout)
+      }
+    }
+  }, [breakpoint])
+
+  return breakpoint
 }
