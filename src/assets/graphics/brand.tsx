5f519e09f5d354cb90fab4aaf672fe8aa00f7bef
Alan Ostrowski <alan.ostrowski2004@gmail.com>
2024-09-26 12:09:58 +0200

    fix: pr suggestions

---
+++ src/assets/graphics/brand.tsx
@@ -0,0 +1,20 @@
+import { useTranslations } from 'next-intl'
+
+type IsMenuOpenProps = {
+  isMenuOpen: boolean
+}
+
+export function Brand({ isMenuOpen }: IsMenuOpenProps) {
+  const t = useTranslations()
+
+  return (
+    <>
+      <div className=' text-gray-300'>{t('sections.navbar.logo.handles')}</div>
+      <div
+        className={`text-red-500 ${isMenuOpen ? '-mt-7' : '-mt-3 md:-mt-4 xl:-mt-5 2xl:-mt-5'}`}
+      >
+        {t('sections.navbar.logo.magnetic')}
+      </div>
+    </>
+  )
+}
--- src/assets/graphics/facebook.tsx
+++ src/assets/graphics/facebook.tsx
@@ -1,9 +1,8 @@
 type FacebookProps = {
   size?: number
-  color: string
 }
 
-function Facebook({ size = 38, color }: FacebookProps) {
+export function Facebook({ size = 38 }: FacebookProps) {
   return (
     <svg width={size} height={size} viewBox='0 0 38 39' fill='none'>
       <path
@@ -12,10 +11,9 @@ function Facebook({ size = 38, color }: FacebookProps) {
       />
       <path
         d='M26.3959 24.9922L27.2383 19.5H21.9688V15.936C21.9688 14.4332 22.7049 12.9688 25.0652 12.9688H27.4609V8.29297C27.4609 8.29297 25.2866 7.92188 23.2078 7.92188C18.8679 7.92188 16.0312 10.5522 16.0312 15.3141V19.5H11.207V24.9922H16.0312V38.2692C17.0133 38.4231 18.0059 38.5002 19 38.5C19.9941 38.5002 20.9867 38.4231 21.9688 38.2692V24.9922H26.3959Z'
-        fill={color}
+        fill='#ffffff'
       />
     </svg>
   )
 }
 
-export default Facebook
