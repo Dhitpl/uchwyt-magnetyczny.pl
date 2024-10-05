e745715453c817a320eb6f94bb5cdde5e98f1bc6
ingrqm <contact@imaxart.com>
2024-10-05 13:04:21 +0200

    refactor: code review requested changes

--- src/utils/deep-merge/deep-merge.ts
+++ src/utils/deep-merge/deep-merge.ts
@@ -25,19 +25,17 @@ export function deepMerge<T>(
   Object.keys(partial).forEach(key => {
     const typedKey = key as keyof T
 
-    if (key in partial) {
-      if (
-        typeof partial[typedKey] === 'object' &&
-        partial[typedKey] !== null &&
-        !Array.isArray(partial[typedKey])
-      ) {
-        result[typedKey] = deepMerge(
-          result[typedKey],
-          partial[typedKey] as DeepPartial<T[keyof T]>,
-        )
-      } else {
-        result[typedKey] = partial[typedKey] as T[keyof T]
-      }
+    if (
+      typeof partial[typedKey] === 'object' &&
+      partial[typedKey] !== null &&
+      !Array.isArray(partial[typedKey])
+    ) {
+      result[typedKey] = deepMerge(
+        result[typedKey],
+        partial[typedKey] as DeepPartial<T[keyof T]>,
+      )
+    } else {
+      result[typedKey] = partial[typedKey] as T[keyof T]
     }
   })
 
