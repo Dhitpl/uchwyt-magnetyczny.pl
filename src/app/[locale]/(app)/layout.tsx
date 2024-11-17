d9f040a09c5902df029e330fee4f5e2ef67db062
ingrqm <contact@imaxart.com>
2024-11-17 23:56:27 +0100

    feat: app layout

--- src/app/[locale]/(app)/layout.tsx
+++ src/app/[locale]/(app)/layout.tsx
@@ -31,10 +31,14 @@ export default function AppLayout({ children }: AppLayoutProps) {
       animate='visible'
       variants={animation.variants}
       transition={animation.transition}
+      className='min-h-screen flex flex-col'
     >
       <Navbar />
+
       <Breadcrumb />
-      {children}
+
+      <main className='flex-1'>{children}</main>
+
       <Footer />
     </motion.div>
   )
