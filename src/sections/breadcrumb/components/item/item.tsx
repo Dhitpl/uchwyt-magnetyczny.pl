8fee8cbd45a79863438299b08cb5c672e21876b4
ingrqm <contact@imaxart.com>
2024-11-17 23:59:44 +0100

    refactor: routing types on breadcrumb

--- src/sections/breadcrumb/components/item/item.tsx
+++ src/sections/breadcrumb/components/item/item.tsx
@@ -1,24 +1,24 @@
 import { Icon, Typography } from '~/components'
 
 import { Link } from '~/i18n/routing'
-import type { Pathname } from '~/i18n/types'
+import type { Href } from '~/i18n/types'
 
 import { cn } from '~/utils'
 
 type BreadcrumbItemProps = {
-  pathname: Pathname
+  href: Href
   isLastSegment: boolean
   label: string
 }
 
 export function BreadcrumbItem({
-  pathname,
+  href,
   isLastSegment,
   label,
 }: BreadcrumbItemProps) {
   return (
     <li className='font-quicksand inline-flex items-center capitalize gap-2.5'>
-      <Link href={pathname}>
+      <Link href={href}>
         <Typography
           variant='label'
           size='md'
@@ -29,6 +29,7 @@ export function BreadcrumbItem({
           {label}
         </Typography>
       </Link>
+
       {isLastSegment === false && (
         <Icon variant='lucide' name='chevron-right' size={18} />
       )}
