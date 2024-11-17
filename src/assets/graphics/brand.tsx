8a922b5964fbb167bdc23f558bb247a326e7da57
ingrqm <contact@imaxart.com>
2024-11-17 23:56:42 +0100

    feat: adjust brand to other graphics

--- src/assets/graphics/brand.tsx
+++ src/assets/graphics/brand.tsx
@@ -1,25 +1,68 @@
 import { useTranslations } from 'next-intl'
+import { DeepPartial } from 'react-hook-form'
 
-import { cn } from '~/utils'
+import { Color, Size } from '~/types'
 
-type IsMenuOpenProps = {
-  isMenuOpen: boolean
+import { useBreakpoint } from '~/hooks'
+
+import { cn, deepMerge, getColor, getSizeByBreakpoint } from '~/utils'
+
+type BrandProps<T extends string = string> = {
+  size?: Size
+  className?: HTMLElement['className']
+  color?: DeepPartial<{
+    top: Color<T>
+    bottom: Color<T>
+  }>
 }
 
-export function Brand({ isMenuOpen }: IsMenuOpenProps) {
+const defaultColor = {
+  top: 'gray-300',
+  bottom: 'red-500',
+}
+
+export function Brand<T extends string>({
+  size = 50,
+  className,
+  color,
+}: BrandProps<T>) {
   const t = useTranslations()
 
+  const breakpoint = useBreakpoint()
+
+  const currentSize = getSizeByBreakpoint({
+    size,
+    breakpoint,
+  })
+
+  const currentColor = deepMerge(defaultColor, color as BrandProps['color'])
+
+  const fontSize = (currentSize || 0) / 2
+
   return (
-    <>
-      <div className=' text-gray-300'>{t('sections.navbar.logo.handles')}</div>
+    <div
+      className={cn('flex flex-col', className)}
+      style={{
+        fontSize: `${fontSize}px`,
+        lineHeight: `${fontSize}px`,
+      }}
+    >
       <div
-        className={cn(
-          'text-red-500',
-          isMenuOpen ? '-mt-7' : '-mt-3 md:-mt-4 xl:-mt-5 2xl:-mt-5',
-        )}
+        className='w-full'
+        style={{
+          color: getColor(currentColor.top as Color),
+        }}
+      >
+        {t('sections.navbar.logo.handles')}
+      </div>
+      <div
+        className='w-full'
+        style={{
+          color: getColor(currentColor.bottom as Color),
+        }}
       >
         {t('sections.navbar.logo.magnetic')}
       </div>
-    </>
+    </div>
   )
 }
